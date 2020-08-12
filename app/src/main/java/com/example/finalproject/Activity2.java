9a47c546a2580b05bf7343e4745b75c7c3fbb0ba
Julian <juliansteinhaussen@web.de>
2020-08-12 16:03:22 +0200

    added Measurement for TouchPrecision in Activity2

---
+++ app/src/main/java/com/example/finalproject/Activity2.java
@@ -0,0 +1,133 @@
+package com.example.finalproject;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+    private Button BluetBtn,RedBtn;
+    private TextView editText,editTextBTNCHoice;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        BluetBtn=(Button) findViewById(R.id.BtnBlue);
+        BluetBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+              //  String text=String.valueOf(findBTN((int)event.getX(),(int)event.getY()));
+                String text=String.valueOf((int)Math.round(Math.sqrt((Math.pow(BluetBtn.getWidth()/2-event.getX(),2)+Math.pow(BluetBtn.getHeight()-event.getY(),2)))));
+                editText.setText(text);
+                return false;
+            }
+        });
+
+        RedBtn=(Button) findViewById(R.id.BtnRed);
+        RedBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                String text=String.valueOf((int)Math.round(Math.sqrt((Math.pow(RedBtn.getWidth()/2-event.getX(),2)+Math.pow(RedBtn.getHeight()-event.getY(),2)))));
+                editText.setText(text);
+                return false;
+            }
+        });
+        editText=(TextView) findViewById(R.id.EditTextTouchPrecision);
+        editTextBTNCHoice=(TextView) findViewById(R.id.EditTextBTNCHOICE);
+
+
+    }
+
+    public int findBTN(int InputX, int InputY){ //returns true if distance between touch pointer and btnBlue(middle of BTN) is smaller then distance between touchpointer and btnRed; else: return false
+        int BlueX=(int) BluetBtn.getX()+BluetBtn.getWidth()/2;
+        int BlueY=(int) BluetBtn.getY()+BluetBtn.getHeight();
+        int RedX=(int) RedBtn.getX()+RedBtn.getWidth()/2;
+        int RedY=(int) RedBtn.getY()+RedBtn.getHeight();
+
+        int distanceBlue=(int)Math.round(Math.sqrt((Math.pow(BlueX-InputX,2)+Math.pow(BlueY-InputY,2))));
+        int distanceRed=(int)Math.round(Math.sqrt((Math.pow(RedX-InputX,2)+Math.pow(RedY-InputY,2))));
+        if(distanceBlue<distanceRed)
+        {   editTextBTNCHoice.setText("BlueBTN");
+            return distanceBlue;}
+        else{
+            editTextBTNCHoice.setText("RedBTN");
+            return distanceRed;}
+
+
+    }
+
+
+
+    //snippet from https://www.vogella.com/tutorials/AndroidTouch/article.html
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        // get pointer index from the event object
+        int pointerIndex = event.getActionIndex();
+
+                // get pointer ID
+        int pointerId = event.getPointerId(pointerIndex);
+
+        // get masked (not specific to a pointer) action
+        int maskedAction = event.getActionMasked();
+
+        int x=(int) event.getX();
+        int y=(int) event.getY();
+        Log.d("x: ", String.valueOf(event.getX()));
+        Log.d("Y: ", String.valueOf(event.getY()));
+        String text=String.valueOf(findBTN(x,y));
+        editText.setText(text);
+
+        switch (maskedAction) {
+
+            case MotionEvent.ACTION_DOWN: // new touch started
+                {
+                // TODO use data
+
+
+                }
+            case MotionEvent.ACTION_POINTER_DOWN: //Pointer down (multi-touch)
+                {
+
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: // a pointer was moved -->Finger is moving  (swipe)
+                {
+                // TODO use data
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: // Finger (swipe) went up
+            {
+
+            }
+            case MotionEvent.ACTION_POINTER_UP: //Pointer up (multi-touch)
+
+            case MotionEvent.ACTION_BUTTON_PRESS: //This action is not a touch event so it is delivered to View#onGenericMotionEvent(MotionEvent) rather than View#onTouchEvent(MotionEvent).
+            { String text2=String.valueOf(findBTN(x,y));
+                editText.setText(text2);}
+            case MotionEvent.ACTION_CANCEL: {
+               break;
+            }
+        }
+
+
+        return true;
+    }
+
+
+}
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,49 +9,40 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button StartBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setBTN();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void setBTN() {
+    StartBtn=(Button) findViewById(R.id.btnStart);
+    StartBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(getBaseContext(), Activity2.class);
+            startActivity(intent);
         }
-
-        return super.onOptionsItemSelected(item);
+    });
     }
+
+    //code snippet from https://developer.android.com/training/gestures/movement
+    // velocity measures speed of touch input after a certain event
+
+
+
+
 }
